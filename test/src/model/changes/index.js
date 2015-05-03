5b55870ee73f5524e47aaa148a1c4750bbf7a0b9
tormozz48 <tormozz48@gmail.com>
2015-05-03 17:10:20 +0300

    Implament tests for changes model

---
+++ test/src/model/changes/index.js
@@ -0,0 +1,31 @@
+var ChangesType = require('../../../../src/model/changes/type'),
+    Changes = require('../../../../src/model/changes');
+
+describe('model/changes', function () {
+    it('should be initialized successfully', function () {
+        new Changes();
+    });
+
+    it('should have docs changes model', function () {
+        var changes = new Changes();
+        changes.should.have.property('docs');
+        changes.docs.should.be.instanceof(ChangesType);
+    });
+
+    it('should have pages changes model', function () {
+        var changes = new Changes();
+        changes.should.have.property('pages');
+        changes.pages.should.be.instanceof(ChangesType);
+    });
+
+    it('should have libraries changes model', function () {
+        var changes = new Changes();
+        changes.should.have.property('libraries');
+        changes.libraries.should.be.instanceof(ChangesType);
+    });
+
+    it('should not be in modified state', function () {
+        var changes = new Changes();
+        changes.areModified().should.not.be.ok;
+    });
+});
