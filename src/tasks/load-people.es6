82ebedce9c34715ce8376e6d0160f2f2c101de36
tormozz48 <tormozz48@gmail.com>
2015-05-11 15:25:21 +0300

    Implement tests for load-people task module

--- src/tasks/load-people.es6
+++ src/tasks/load-people.es6
@@ -34,19 +34,19 @@ export default class LoadPeople extends Base {
         this.logger.debug(`to: ==> ${destinationPath}`);
 
         return new Promise((resolve, reject) => {
-            request
-                .get(this.getTaskConfig().url)
-                .pipe(fs.createWriteStream(destinationPath))
-                .on('error', error => {
-                    this.logger.error('Error occur while loading people.json file');
-                    this.logger.error(error.message);
-                    reject(error);
-                })
-                .on('close', () => {
-                    this.logger.debug('people.json file was loaded successfully and saved to cache');
-                    model.initPeople(fsExtra.readJSONSync(destinationPath));
-                    resolve(model);
-                });
+            request({ url: this.getTaskConfig().url, json: true }, (error, response, body) => {
+                if (error || response.statusCode !== 200) {
+                    let errorMessage = 'Error occur while loading people.json file';
+                    this.logger.error(errorMessage);
+                    this.logger.error(error ? error.message : 'Error');
+                    return reject(new Error(errorMessage));
+                }
+                fsExtra.writeJSONSync(destinationPath, body);
+                this.logger.debug('people.json file was loaded successfully and saved to cache');
+
+                model.initPeople(body);
+                return resolve(model);
+            });
         });
     }
 }
--- test/src/tasks/load-people.js
+++ test/src/tasks/load-people.js
@@ -1,10 +1,58 @@
-var path = require('path'),
+var fs = require('fs'),
+    path = require('path'),
     should = require('should'),
+    fsExtra = require('fs-extra'),
     Config = require('../../../lib/config'),
+    Model = require('../../../lib/model/model'),
     LoadPeople = require('../../../lib/tasks/load-people');
 
 describe('LoadPeople', function () {
+    before(function () {
+        process.chdir(path.resolve(__dirname, '../../stub'));
+    });
 
+    describe('invalid url', function () {
+        var peopleUrl = 'https://raw.githubusercontent.com/bem/bem-method/bem-info-data/people/people1.json',
+            task,
+            config;
+
+        before(function () {
+            config = new Config();
+            task = new LoadPeople(config, { url: peopleUrl });
+            fsExtra.mkdirpSync(config.getCacheDirPath());
+        });
+
+        it('run', function (done) {
+            var model = new Model();
+            task.run(model).catch(function (error) {
+                fs.existsSync('./cache/people.json').should.equal(false);
+                done();
+            });
+        });
+    });
+
+    describe('success', function () {
+        var peopleUrl = 'https://raw.githubusercontent.com/bem/bem-method/bem-info-data/people/people.json',
+            task,
+            config;
+
+        before(function () {
+            config = new Config();
+            task = new LoadPeople(config, { url: peopleUrl });
+            fsExtra.mkdirpSync(config.getCacheDirPath());
+        });
+
+        it('run', function (done) {
+            var model = new Model();
+            task.run(model).then(function () {
+                fs.existsSync('./cache/people.json').should.equal(true);
+                done();
+            });
+        });
+    });
+
+    after(function () {
+        fsExtra.removeSync('./cache');
+        process.chdir(path.resolve(__dirname, '../../../'));
+    });
 });
-
-
