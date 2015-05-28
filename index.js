74439a9f14cfe6d404b920458e7fcad67fa39975
tormozz48 <tormozz48@gmail.com>
2015-05-29 00:06:42 +0300

    Add main index file

---
+++ index.js
@@ -0,0 +1,12 @@
+exports.Builder = require('./lib/index');
+
+exports.tasks = {
+    Base: require('./lib/tasks/base'),
+    AnalyzeModel: require('./lib/tasks/analyze-model'),
+    LoadModelFiles: require('./lib/tasks/load-model-files'),
+    MakeDirectory: require('./lib/tasks/make-directory'),
+    MakePagesCache: require('./lib/tasks/make-pages-cache'),
+    MergeModels: require('./lib/tasks/merge-models'),
+    SaveDataFile: require('./lib/tasks/save-data-file'),
+    SaveModelFile: require('./lib/tasks/save-model-file')
+};
