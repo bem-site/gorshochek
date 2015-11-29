f83019ee3354b78ca362152171265581a1e36251
tormozz48 <tormozz48@gmail.com>
2015-11-29 22:48:11 +0300

    Rewrite index.js file for providing all ready modules

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 exports.Builder = require('./lib/index');
 
 exports.tasks = {
-    Base: require('./lib/tasks/base'),
-    Init: require('./lib/tasks/init'),
-    Finalize: require('./lib/tasks/finalize')
+    core: {
+        Base: require('./lib/tasks-core/base'),
+        Init: require('./lib/tasks-core/init'),
+        RsyncCacheData: require('./lib/tasks-core/rsync-cache-data')
+    },
+    docs: {
+        LoadFromGithub: require('./lib/tasks-docs/load-from-github'),
+        LoadFromFile: require('./lib/tasks-docs/load-from-file'),
+        TransformBase: require('./lib/tasks-docs/transform-base'),
+        TransformMdToHtml: require('./lib/tasks-docs/transform-md-to-html')
+    },
+    libraries: {},
+    page: {
+        Base: require('./lib/tasks-page/base'),
+        Breadcrumbs: require('./lib/tasks-page/breadcrumbs'),
+        HeaderMeta: require('./lib/tasks-page/header-meta'),
+        HeaderTitle: require('./lib/tasks-page/header-title'),
+        SearchMeta: require('./lib/tasks-page/search-meta')
+    },
+    sitemap: {
+        SitemapXML: require('./lib/tasks-sitemap/sitemap-xml')
+    }
 };
